4d114ee7bc345507251f474f949f67952105ed28
Abde lilah <85647797+0xGLSS@users.noreply.github.com>
2025-02-02 21:57:20 +0000

    Create secrets-test.js

---
+++ secrets-test.js
@@ -0,0 +1,68 @@
+# Test file with various secrets for testing truffle.py
+
+# Slack
+slack_token = "xoxp-123456789012-123456789012-123456789012-abcdef1234567affa890abfcdef1234567890"
+
+# Keys
+RSA_KEY = """-----BEGIN RSA PRIVATE KEY-----
+MIIBOgIBAAJBAKj34GkxFhD90vcNLYLInFEX6Ppy1tPf9Cnzj4p4WGeKLs1Pt8Qu
+KUpRKfFLfRYC9AIKjbJTWit+CqvjWYzvQwECAwEAAQJAIJLixBy2qpFoS4DSmoEmazfff
+...
+-----END RSA PRIVATE KEY-----"""
+ffaff
+# AWSs
+aws_key = "AKIAIOSFODNN7EXAMPLE"
+aws_token = "amzn.mws.12345678-1234-1234-1234-123456789012"
+aws_appsync = "da2-12345678901234567890123456"
+ff
+# Facebook
+fb_token = "EAACEdEose0cBAQualified123456789"
+fb_oauth = 'facebook_secret="12345678901234567890123456789012"'
+
+# GitHub
+github_key = 'github_token="1234567890123456789012345678901234567azrar890"'
+github_auth = "https://1234567890123456789012345678901234567890@github.com"
+
+# Google
+google_service = """
+{azfazf
+  "type": "service_account",
+  "project_id": "test"
+}
+"""
+google_api = "AIza1234567890-abcdefghijklmnopqrstuvwxyz12345"
+google_oauth = "123456789012-abcdefghijklmnopqrstuvwxyz123456.apps.googleusercontent.com"
+google_gmail = "ya29.ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklm-nopqrstuvwxyz0123"
+
+# Heroku
+heroku_key = "heroku_key='12345678-ABCD-EFGH-IJKL-123456789012'"
+
+# JWT
+jwt_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIn0"
+
+# Mail Services
+mailchimp = "abcdef1234567890abcdef1234567890-us1"
+mailgun = "key-abcdef1234567890abcdef1234567890"
+
+# URLs with passwords
+db_url = "postgres://user:password123@localhost:5432/db"
+
+# Payment Services
+paypal = "access_token$production$1234567890123456$1234567890abcdef1234567890abcdef"
+stripe_key = "sk_live_12345678901234567890abcd"
+stripe_restricted = "rk_live_12345678901234567890abcd"
+square_token = "sq0atp-12345678901234567890-12"
+square_oauth = "sq0csp-12345678901234567890123456789012345678901234"
+
+# Chat Services
+telegram = "1234567890:AAA1234567890123456789012345678901234"
+slack_webhook = "https://hooks.slack.com/services/T12345678/B12345678/123456789012345678901234"
+twilio = "SK12345678901234567890123456789012"
+
+# Twitter
+twitter_token = "twitter_key_1234567890-abcdefghijklmnopqrstuvwxyz12345"
+twitter_oauth = 'twitter_secret="1234567890abcdefghijklmnopqrstuvwxyz12345"'
+
+# Generic API/Secret patterns
+api_key = 'api_key="1234567890abcdef1234567890abcdef123"'
+secret_key = 'secret="1234567890abcdef1234567890abcdef123"' 
